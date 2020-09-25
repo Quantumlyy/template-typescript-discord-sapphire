7058c59148d401ffd8447ebbc75398bd6bab7825
QuantumlyTangled <ethoarx@gmail.com>
2020-09-25 22:15:25 +0200

    feat(client): basic structure

---
+++ src/lib/BotClient.ts
@@ -0,0 +1,7 @@
+import { SapphireClient } from '@sapphire/framework';
+
+import '@scp/in17n/register';
+
+export class BotClient extends SapphireClient {
+
+}
---
+++ src/lib/utils/utils.ts
@@ -0,0 +1,11 @@
+import { Events } from '@sapphire/framework';
+import { isThenable } from '@sapphire/utilities';
+import { BotClient } from '../BotClient';
+
+/**
+ * @copyright 2019-2020 Antonio Román
+ * @license Apache-2.0
+ */
+export function floatPromise(ctx: { client: BotClient }, promise: Promise<unknown>) {
+	if (isThenable(promise)) promise.catch(error => ctx.client.emit(Events.Error, error));
+}
