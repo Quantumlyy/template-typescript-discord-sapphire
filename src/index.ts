cad8dc5d8101417d1ebdff57c77a689c9335c783
QuantumlyTangled <ethoarx@gmail.com>
2020-10-04 09:40:32 +0200

    feat: start bot in main

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import 'module-alias/register';
+import { BotClient } from '@lib/BotClient';
+import { BOT_TOKEN, CLIENT_OPTIONS } from '@root/config';
+import { floatPromise } from '@utils/utils';
 
-export const main = () => 'this builds and pushes';
+const client = new BotClient(CLIENT_OPTIONS);
+
+async function main() {
+	await client.login(BOT_TOKEN);
+}
+
+floatPromise({ client }, main());
+
+export const mainTest = () => 'this builds and pushes';
